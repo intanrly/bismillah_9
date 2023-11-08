8ad7b4406a296405050b1ab13d2caab6c0e9e535
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-08 21:33:00 +0700

    a

--- bismillahbisa.php
+++ bismillahbisa.php
@@ -145,63 +145,63 @@
         </form>
 
         <div id="response">
-        <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $Id = $_POST["Id"];
-            $F_Name = $_POST["F_Name"];
-            $L_Name = $_POST["L_Name"];
-            $Email = $_POST["Email"];
-            $Email2 = $_POST["Email2"];
-            $Profesi = $_POST["Profesi"];
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $Id = $_POST["Id"];
+                $F_Name = $_POST["F_Name"];
+                $L_Name = $_POST["L_Name"];
+                $Email = $_POST["Email"];
+                $Email2 = $_POST["Email2"];
+                $Profesi = $_POST["Profesi"];
 
-            if ($Email != Email2) {
-                echo "Email tidak sama, silahkan coba lagi!";
+                if ($Email != Email2) {
+                    echo "Email tidak sama, silahkan coba lagi!";
+                } else {
+                    $data = '$Id, $F_Name, $L_Name, $Email, $Email2, $Profesi\n';
+                    file_put_contents('C:\Intan\TSD UNAIR\Semester 3\Alpro 2\week 9\datapribadi.csv', $data, FILE_APPEND);
+                    echo 'Data telah ditambahkan!';
+                }
+            }
+            ?>
+
+            <?php
+            echo "<table border= '1'>
+            <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Profesi</th>
+            </tr>";
+
+            $csvfile = 'C:\Intan\TSD UNAIR\Semester 3\Alpro 2\week 9\datapribadi.csv';
+
+            if (is_writable($csvfile)) {
+                // Izin penulisan ada, lanjutkan menulis ke file
+                // ...
             } else {
-                $data = '$Id, $F_Name, $L_Name, $Email, $Email2, $Profesi\n';
-                file_put_contents('C:\Intan\TSD UNAIR\Semester 3\Alpro 2\week 9\datapribadi.csv', $data, FILE_APPEND);
-                echo 'Data telah ditambahkan!';
+                // Izin penulisan tidak ada, tampilkan pesan kesalahan
+                echo 'Tidak bisa menulis ke file.';
             }
-        }
-        ?>
 
-        <?php
-        echo "<table border= '1'>
-        <tr>
-        <th>Id</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Profesi</th>
-        </tr>";
+            if (file_exists($csvfile)) {
+                $csv = array_map('str_getcsv', file($csvfile));
 
-        $csvfile = 'C:\Intan\TSD UNAIR\Semester 3\Alpro 2\week 9\datapribadi.csv';
-
-        if (is_writable($csvfile)) {
-            // Izin penulisan ada, lanjutkan menulis ke file
-            // ...
-        } else {
-            // Izin penulisan tidak ada, tampilkan pesan kesalahan
-            echo 'Tidak bisa menulis ke file.';
-        }
-
-        if (file_exists($csvfile)) {
-            $csv = array_map('str_getcsv', file($csvfile));
-
-            foreach ($csv as $row) {
-                echo "<tr>";
-                echo "<td>" . $row[0] . "</td>";
-                echo "<td>" . $row[1] . "</td>";
-                echo "<td>" . $row[2] . "</td>";
-                echo "<td>" . $row[3] . "</td>";
-                echo "<td>" . $row[4] . "</td>";
-                echo "</tr>";
+                foreach ($csv as $row) {
+                    echo "<tr>";
+                    echo "<td>" . $row[0] . "</td>";
+                    echo "<td>" . $row[1] . "</td>";
+                    echo "<td>" . $row[2] . "</td>";
+                    echo "<td>" . $row[3] . "</td>";
+                    echo "<td>" . $row[4] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "File CSV Tidak Ada!";
             }
-        } else {
-            echo "File CSV Tidak Ada!";
-        }
 
-        echo "</table>";
-        ?>
+            echo "</table>";
+            ?>
         </div>
     </div>
 
