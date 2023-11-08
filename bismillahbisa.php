4586f5d07fa982495bfca20ecddbe1400d03eee2
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-08 21:20:58 +0700

    o

--- bismillahbisa.php
+++ bismillahbisa.php
@@ -145,42 +145,8 @@
         </form>
 
     <div id="response">
-        <?php
-        // Pastikan ini adalah file CSV yang benar-benar ada
-        $csvfile = 'C:\xampp\htdocs\week9\coba.csv';
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Terima data dari formulir
-            $Id = $_POST['Id'];
-            $F_Name = $_POST['F_Name'];
-            $L_Name = $_POST['L_Name'];
-            $Email = $_POST['Email'];
-            $Email2 = $_POST['Email2'];
-            $Profesi = $_POST['Profesi'];
-            // Dapatkan data dari bidang lainnya
-
-            // Validasi data jika diperlukan
-            if (empty($Id) || empty($F_Name) || empty($L_Name) || empty($Email) || empty($Email2) || empty($Profesi)) {
-                $response = array('success' => false, 'message' => 'Data tidak lengkap.');
-            } else {
-                // Simpan data ke file CSV (tambahkan validasi dan pengolahan data lainnya jika diperlukan)
-                $data = "$Id, $F_Name, $L_Name, $Email, $Email2, $Profesi\n";
-                file_put_contents($csvfile, $data, FILE_APPEND);
-                $response = array('success' => true, 'message' => 'Data berhasil disimpan.');
-            }
-
-            header('Content-Type: application/json');
-            echo json_encode($response);
-        } else {
-            // Tampilkan data dari file CSV sebagai respons API
-            $csv = array_map('str_getcsv', file($csvfile));
-            header('Content-Type: application/json');
-            echo json_encode($csv);
-        }
-        ?>
         <?php>
-
-        $csvfile = 'C:\xampp\htdocs\week9\coba.csv';
+        $csvfile = 'C:\xampp\htdocs\week9\datapribadi.csv';
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $Id = $_POST["Id"];
@@ -194,13 +160,10 @@
                 echo "Email tidak sama, silahkan coba lagi!";
             } else {
                 $data = '$Id, $F_Name, $L_Name, $Email, $Email2, $Profesi\n';
-                file_put_contents('C:\xampp\htdocs\week9\coba.csv', $data, FILE_APPEND);
+                file_put_contents('C:\xampp\htdocs\week9\datapribadi.csv', $data, FILE_APPEND);
                 echo 'Data telah ditambahkan!';
             }
         }
-        ?>
-
-        <?php
         echo "<table border= '1'>
         <tr>
         <th>Id</th>
