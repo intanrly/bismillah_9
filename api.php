db1f1064da9957c47933afe074a4b96c71af6ef4
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-10 19:58:04 +0700

    a

--- api.php
+++ api.php
@@ -4,30 +4,23 @@ $csvfile = 'datapribadi.csv';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Terima data dari formulir
-    $Id = $_POST['Id'];
+    $Id = $_POST['id'];
     $F_Name = $_POST['F_Name'];
     $L_Name = $_POST['L_Name'];
-    $Email = $_POST['Email'];
-    $Email2 = $_POST['Email2'];
-    $Profesi = $_POST['Profesi'];
+    $Email = $_POST['email'];
+    $Email2 = $_POST['email2'];
+    $Profesi = $_POST['profesi'];
     // Dapatkan data dari bidang lainnya
 
-    // Validasi data jika diperlukan
-    if (empty($Id) || empty($F_Name) || empty($L_Name) || empty($Email) || empty($Email2) || empty($Profesi)) {
-        $response = array('success' => false, 'message' => 'Data tidak lengkap.');
-    } else {
-        // Simpan data ke file CSV (tambahkan validasi dan pengolahan data lainnya jika diperlukan)
-        $data = "$Id, $F_Name, $L_Name, $Email, $Email2, $Profesi\n";
-        file_put_contents($csvfile, $data, FILE_APPEND);
-        $response = array('success' => true, 'message' => 'Data berhasil disimpan.');
+    $list = array (
+        array($id,$firstName,$lastName,$email,$email2,$profesi)
+    );
+
+    $file = fopen('datapribadi.csv','a');
+    foreach ($list as $fields) {
+        fputcsv($file, $fields);
     }
 
-    header('Content-Type: application/json');
-    echo json_encode($response);
-} else {
-    // Tampilkan data dari file CSV sebagai respons API
-    $csv = array_map('str_getcsv', file($csvfile));
-    header('Content-Type: application/json');
-    echo json_encode($csv);
+    fclose($file);
 }
 ?>
