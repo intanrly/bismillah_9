9f97888deeb70e8ef89d9edcbc488cad740c1822
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-10 20:00:50 +0700

    a

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ new DataTable('#example', {
     ajax: 'http://intanurul04.alwaysdata.net/bismillah_9/getcsv2json.php',
     columns: [
         { data: 'id' },
-        { data: 'F_Name' },
-        { data: 'L_Name"' },
+        { data: 'F_name' },
+        { data: 'L_name"' },
         { data: 'email' },
         { data: 'email2' },
         { data: 'profesi' }
