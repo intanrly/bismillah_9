f2d3a39f825d05dd48cd233d1781e4c971667276
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-11 20:16:19 +0700

    aa

---
+++ cobadoang.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PRAKTIKUM WEEK 9 ALGORITMA PEMROGRAMAN 2</title>
+</head>
+<body>
+    <h1>PRAKTIKUM WEEK 9 ALGORITMA PEMROGRAMAN 2</h1>
+    <h2>INTAN NURUL LAILY - 164221060</h2>
+    <h3>Post Data</h3>
+
+    <div id="container">
+        <form id="addForm" method="post" action="">
+            <label for="id">Id:</label>
+            <input type="text" id="id" name="id" required><br><br>
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" required><br><br>
+            <label for="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" required><br><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="email2">Email2:</label>
+            <input type="email" id="email2" name="email2" required><br><br>
+            <label for="profesi">Profesi:</label>
+            <input type="text" id="profesi" name="profesi" required><br><br>
+            <input type="submit" id="submit" value="Submit">
+        </form>
+    </div>
+
+    <label for="searchInput">Search:</label>
+    <input type="text" id="searchInput" oninput="searchTable()" placeholder="Cari berdasarkan ID atau Nama...">
+
+    <table id="mahasiswaTable">
+        <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Email2</th>
+            <th>Profesi</th>
+        </tr>
+        <?php
+        // Memproses data yang dikirim dari formulir
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $id = $_POST["id"];
+            $firstName = $_POST["firstName"];
+            $lastName = $_POST["lastName"];
+            $email = $_POST["email"];
+            $email2 = $_POST["email2"];
+            $profesi = $_POST["profesi"];
+
+            // Menambahkan data ke dalam tabel
+            echo "<tr>";
+            echo "<td>" . $id . "</td>";
+            echo "<td>" . $firstName . "</td>";
+            echo "<td>" . $lastName . "</td>";
+            echo "<td>" . $email . "</td>";
+            echo "<td>" . $email2 . "</td>";
+            echo "<td>" . $profesi . "</td>";
+            echo "</tr>";
+        }
+
+        // Membaca data dari file CSV
+        $file = fopen("datapribadi.csv", "r");
+        if ($file) {
+            while (($data = fgetcsv($file, 1000, ",")) !== false) {
+                echo "<tr>";
+                foreach ($data as $cell) {
+                    echo "<td>" . $cell . "</td>";
+                }
+                echo "</tr>";
+            }
+            fclose($file);
+        }
+        ?>
+    </table>
+
+    <script>
+        function searchTable() {
+            const input = document.getElementById('searchInput').value.toLowerCase();
+            const table = document.getElementById('mahasiswaTable');
+            const rows = table.getElementsByTagName('tr');
+
+            for (let i = 1; i < rows.length; i++) {
+                const cells = rows[i].getElementsByTagName('td');
+                let found = false;
+
+                for (let j = 0; j < cells.length; j++) {
+                    const cellText = cells[j].textContent.toLowerCase();
+                    if (cellText.includes(input)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found) {
+                    rows[i].style.display = '';
+                } else {
+                    rows[i].style.display = 'none';
+                }
+            }
+        }
+    </script>
+</body>
+</html>
